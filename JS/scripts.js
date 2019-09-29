b13cdc568cf5cb3718f063d96a542502777c0dad
NzauM <mercywaenu16@gmail.com>
2019-09-29 14:25:13 +0300

    Add a click listener to the submit button and give submit a function

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,6 +1,3 @@
-
-
-
 $(document).ready(function(){
     $("#design").click(function(){
         $("#designs").toggle();
@@ -19,11 +16,33 @@ $("#developments").toggle();
         $("#about").slideDown("slow");
     });
 
-    $("#submit").click(function(){
-        $("alert")("Message received");
-    });
+    // $("#submit").click(function(){
+    //     $("alert")("Message received");
+    // });
 
     // $(".form").submit(function(){
     //     alert("Message received" );
     // });
+    // $("#submit").click(function(submit){
+    //     alert("Message received" + name);
+    // });
+// });
+
+    $("#submit").click(function(){
+        submit();
+        event.preventDefault();
+    })
+        
 });
+
+function submit(){
+    var name=document.getElementById("name").value;
+    var messages = document.getElementById("message").value;
+    if(name,messages == ''){
+        alert("Please fill out the fields")
+    }
+    else{
+    alert("Message received" + ' ' + name);
+    }
+};
+
